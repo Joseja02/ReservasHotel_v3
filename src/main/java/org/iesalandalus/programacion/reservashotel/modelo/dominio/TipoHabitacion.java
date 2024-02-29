593257fc179193ba60ac0e98f3a42b4d20a73d54
Jose Javier Sierra Berdun <jsieber1908@gmail.com>
2024-02-29 20:31:10 +0100

    TipoHabitacion actualizado

--- src/main/java/org/iesalandalus/programacion/reservashotel/modelo/dominio/TipoHabitacion.java
+++ src/main/java/org/iesalandalus/programacion/reservashotel/modelo/dominio/TipoHabitacion.java
@@ -1,25 +1,19 @@
 package org.iesalandalus.programacion.reservashotel.modelo.dominio;
 
 public enum TipoHabitacion {
-    SUITE("SUITE", 4),
-    SIMPLE("SIMPLE", 1),
-    DOBLE("DOBLE", 2),
-    TRIPLE("TRIPLE", 3);
+    SUITE("SUITE"),
+    SIMPLE("SIMPLE"),
+    DOBLE("DOBLE"),
+    TRIPLE("TRIPLE");
 
     private final String cadenaAMostrar;
-    private final int numeroMaximoPersonas;
-    private TipoHabitacion(String cadenaAMostrar, int numeroMaximoPersonas){
+    private TipoHabitacion(String cadenaAMostrar){
         this.cadenaAMostrar = cadenaAMostrar;
-        this.numeroMaximoPersonas = numeroMaximoPersonas;
-    }
-    public int getNumeroMaximoPersonas() {
-        return numeroMaximoPersonas;
     }
     @Override
     public String toString() {
         return "TipoHabitacion{" +
                 "cadenaAMostrar='" + cadenaAMostrar + '\'' +
-                ", numeroMaximoPersonas=" + numeroMaximoPersonas +
                 '}';
     }
 }
